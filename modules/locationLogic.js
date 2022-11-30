585ff7609d9406c7c4cbf12111dada4a4aeab4ed
Lenni009 <guentherhanslphdyt@gmail.com>
2022-11-30 22:21:46 +0100

    add planetMoon function

--- modules/locationLogic.js
+++ modules/locationLogic.js
@@ -1,7 +1,15 @@
-function planetMoon(planet, moon) {
-	if (moon) {
-		return `moon ${moon}`;
-	} else {
-		return `planet ${planet}`;
-	}
+function planetMoon() {
+	const planetElement = globalElements.input.planetInput;
+	const moonElement = globalElements.input.moonInput;
+	const planet = planetElement.value;
+	const moon = moonElement.value;
+	const dest = globalElements.output.celestialBody;
+	const text = (() => {
+		if (moon) {
+			return `moon [[${moon}]]`;
+		} else {
+			return `planet [[${planet}]]`;
+		}
+	})();
+	dest.innerText = text;
 }
