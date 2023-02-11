1b0e0feefd4ff7ce253ef7b1a89b6ddb6f4d0df0
Lenni009 <guentherhanslphdyt@gmail.com>
2023-02-11 01:34:11 +0100

    activate moon

---
+++ src/pages/moon.js
@@ -0,0 +1,16 @@
+function galleryExplanationExternal() {
+	return `There is a preferred order of pictures:
+	<div class='dialog-center'>
+		<ol class='dialog-list'>
+			<li>Landscape</li>
+			<li>Night View</li>
+			<li>Cave System</li>
+			<li>Analysis Visor</li>
+			<li>Moon Exploration Guide</li>
+			<li>Moon Page</li>
+			<li>Planet Page</li>
+			<li>System Page</li>
+			<li>Galaxy Map</li>
+		</ol>
+	</div>`
+}
