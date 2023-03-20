5700bbe39cac4f77f766acdaf9d86425d0aee096
Lenni009 <guentherhanslphdyt@gmail.com>
2023-03-20 21:38:31 +0000

    Deploying to gh-pages from @ Lenni009/HubWikiPageCreator@3352bab3314ee558bf6938df9d3d83c6e4a8261a 🚀

---
+++ src/misc_logic/dateLogic.js
@@ -0,0 +1,1 @@
+function processDate(e){const t=e.value,o=e.dataset.destNoauto;wikiCode(t.replaceAll("-","/"),o)}function getCurrentYear(e){const t=(new Date).getFullYear();wikiCode(t.toString(),e)}
