3d0780cb1ec25d002d6a1bac9f175f8cbf89bba0
Lenni009 <guentherhanslphdyt@gmail.com>
2023-03-25 20:59:04 +0100

    fix JSDoc

--- src/common.js
+++ src/common.js
@@ -1220,7 +1220,7 @@ function shortenGHub(civ) {
  * @param {HTMLElement} element - The HTML element to calculate statistics for.
  * @param {number} [decimals=null] - The number of decimal places to use in formatted results. If `null`, the function will use the default number of decimal places.
  * @param {boolean} [outputRaw=false] - Whether to output the raw calculation values along with the formatted results. If `true`, the function will return an object with `formatted` and `raw` properties; if `false`, it will return only the formatted string.
- * @returns {string|object} The formatted statistics string (and optionally, the raw calculation values).
+ * @returns {string|Object} The formatted statistics string (and optionally, the raw calculation values).
  */
 function numberStats(element, decimals = null, outputRaw = false) {
 	if (arguments.length == 0) {
