f389c2b88471247d48aa61ef7a1731c8306fe1a6
Lenni009 <guentherhanslphdyt@gmail.com>
2023-06-10 10:09:11 +0200

    prepare for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+/// <reference types="vitest" />
+
 import { resolve } from 'path';
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "./",
+	test: {
+		browser: {
+			enabled: true,
+//			headless: true,
+			provider: 'playwright', 
+			name: 'firefox',
+		  },
+	},
 	build: {
 		rollupOptions: {
 			input: {
