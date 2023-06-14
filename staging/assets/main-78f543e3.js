80f81bebf940529727e5b95947dc5027211045bb
Lenni009 <guentherhanslphdyt@gmail.com>
2023-06-14 20:24:48 +0000

    Deploying to gh-pages from @ Lenni009/HubWikiPageCreator@3f2ec7de48079edc76d669c420bd24822cb8ddea 🚀

---
+++ staging/assets/main-78f543e3.js
@@ -0,0 +1,1 @@
+import"./about-b18469d8.js";const c=["wiki.nmsgalactichub.com","nmswikipagecreator.nmsgalactichub.com","hubwikipagecreator.nmsgalactichub.com"];if(c.includes(window.location.host)){const c=document.querySelector('a[href=".."]');c&&(c.href="https://lenni009.github.io/")}
