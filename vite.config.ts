8abbcb7e683dbd6faba040adaa51cdff390d4909
Lenni009 <guentherhanslphdyt@gmail.com>
2023-09-27 21:34:29 +0200

    adjust staging paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/EisvanaWikiPageCreator/",
+	base: "/EisvanaWikiPageCreator/staging/",
 	test: {
 		browser: {
 			enabled: true,
