110ec25da7c57ef222d13ee8bd59239e5837ae6a
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2023-11-19 21:02:27 +0100

    Patch 1
    Added new pages in spanish

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/WikiPageCreator/',
+  base: '/EisvanaWikiPageCreator/',
   plugins: [vue()],
   test: {
     browser: {
