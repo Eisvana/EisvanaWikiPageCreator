a75e9998574a029d1ae0305f0cf2184136055847
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2023-11-20 22:45:13 +0100

    Update versions.ts

--- src/variables/versions.ts
+++ src/variables/versions.ts
@@ -1,6 +1,36 @@
 /**
  * An array of version names for the game: No Man's Sky
  *
- *
- *
+ * @type {string[]}
+ * @constant
  */
+export const versions = {[key: string]: string} = {
+	Echoes: 'Echoes',
+	Singularity: 'Singularidad',
+	Interceptor: 'Interceptor',
+	Fractal: 'Fractal',
+  Waypoint: 'Waypoint',
+  Endurance: 'Endurance',
+  Outlaws: 'Outlaws',
+  SentinelUp: 'Sentinel',
+  Frontiers: 'Frontiers',
+  Prisms: 'Prisms',
+  Expeditions: 'Expediciones',
+  Companions: 'Compañeros',
+  NextGen: 'Nueva Generación',
+  Origins: 'Origins',
+  Desolation: 'Desolation',
+  Crossplay: 'Crossplay',
+  ExoMech: 'ExoMech',
+  'Living Ship': 'Living Ship',
+  Synthesis: 'Sintesis',
+  Beyond: 'Mas Alla',
+  Visions: 'Visiones',
+  Abyss: 'Abismo',
+  NEXT: 'NEXT',
+  'Atlas Rises': 'Atlas Rises',
+  Pathfinder: 'Pathfinder',
+  Foundation: 'Fundación',
+  Release: 'Lanzamiento',
+}
+
