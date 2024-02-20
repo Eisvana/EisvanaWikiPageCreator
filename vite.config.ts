32f6a9bfd6fc31c069b4284c128c25bd86ebd5e3
Lenni009 <guentherhanslphdyt@gmail.com>
2024-02-20 20:37:43 +0100

    stop using __dirname

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest" />
 import { fileURLToPath, URL } from 'node:url';
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
@@ -19,22 +18,22 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        base: resolve(__dirname, 'base.html'),
-        biofrig: resolve(__dirname, 'biofrig.html'),
-        derelict: resolve(__dirname, 'derelict.html'),
-        fauna: resolve(__dirname, 'fauna.html'),
-        flora: resolve(__dirname, 'flora.html'),
-        mineral: resolve(__dirname, 'mineral.html'),
-        moon: resolve(__dirname, 'moon.html'),
-        multitool: resolve(__dirname, 'multitool.html'),
-        planet: resolve(__dirname, 'planet.html'),
-        racetrack: resolve(__dirname, 'racetrack.html'),
-        sandworm: resolve(__dirname, 'sandworm.html'),
-        settlement: resolve(__dirname, 'settlement.html'),
-        starship: resolve(__dirname, 'starship.html'),
-        system: resolve(__dirname, 'system.html'),
+        main: fileURLToPath(new URL('index.html', import.meta.url)),
+        about: fileURLToPath(new URL('about.html', import.meta.url)),
+        base: fileURLToPath(new URL('base.html', import.meta.url)),
+        biofrig: fileURLToPath(new URL('biofrig.html', import.meta.url)),
+        derelict: fileURLToPath(new URL('derelict.html', import.meta.url)),
+        fauna: fileURLToPath(new URL('fauna.html', import.meta.url)),
+        flora: fileURLToPath(new URL('flora.html', import.meta.url)),
+        mineral: fileURLToPath(new URL('mineral.html', import.meta.url)),
+        moon: fileURLToPath(new URL('moon.html', import.meta.url)),
+        multitool: fileURLToPath(new URL('multitool.html', import.meta.url)),
+        planet: fileURLToPath(new URL('planet.html', import.meta.url)),
+        racetrack: fileURLToPath(new URL('racetrack.html', import.meta.url)),
+        sandworm: fileURLToPath(new URL('sandworm.html', import.meta.url)),
+        settlement: fileURLToPath(new URL('settlement.html', import.meta.url)),
+        starship: fileURLToPath(new URL('starship.html', import.meta.url)),
+        system: fileURLToPath(new URL('system.html', import.meta.url)),
       },
     },
   },
