69a481f3ed7e2b2ad328319963878f64172e39c0
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-01 21:07:43 +0200

    add component for image display on main page

---
+++ src/components/CivImage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { CivImageProps } from '@/types/homePageProps';
+
+defineProps<CivImageProps>();
+</script>
+
+<template>
+  <a
+    :href="link"
+    rel="noopener noreferrer"
+    target="_blank"
+  >
+    <img
+      :alt="imgAlt"
+      :src="img"
+    />
+  </a>
+</template>
+
+<style scoped lang="scss">
+img {
+  width: 200px;
+}
+</style>
