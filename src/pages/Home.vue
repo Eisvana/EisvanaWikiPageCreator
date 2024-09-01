0ca81e6ad0e0c8038a8eee12b1cb1f89f88ece61
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-01 21:11:46 +0200

    use quasar components for home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-import PageLink from '@/components/structure/PageLink.vue';
-import BuiltBy from '@/components/structure/BuiltBy.vue';
-import CivImage from '@/components/structure/CivImage.vue';
-import type { BuiltByProps, CivImageProps, PageLinkProps } from '@/types/objects';
+import type { CivImageProps, PageLinkProps } from '@/types/homePageProps';
+import CivImage from '@/components/CivImage.vue';
 
 const links: PageLinkProps[] = [
   {
@@ -49,10 +47,6 @@ const links: PageLinkProps[] = [
     text: 'Organic Frigate',
     url: './biofrig.html',
   },
-  // {
-  //   text: 'Artifact',
-  //   url: './artifact.html',
-  // },
   {
     text: 'System',
     url: './system.html',
@@ -67,59 +61,32 @@ const links: PageLinkProps[] = [
   },
 ];
 
-const builtByCredits: BuiltByProps[] = [
-  {
-    text: 'Eisvana Wiki Scholars',
-    // link: 'https://nomanssky.fandom.com/wiki/Eisvana_Wiki_Scholars',
-    // img: '',
-    // imgAlt: 'Eisvana Wiki Scholars logo',
-  },
-  {
-    text: "Assistant for No Man's Sky",
-    link: 'https://nmsassistant.com/',
-    img: 'https://cdn.assistantapps.com/assistantNMS.png',
-    imgAlt: 'Assistant for NMS logo',
-  },
-];
-
 const images: CivImageProps[] = [
   {
     link: 'https://nomanssky.fandom.com/wiki/Eisvana',
     imgAlt: 'Eisvana logo',
     img: 'https://static.wikia.nocookie.net/nomanssky_gamepedia/images/8/85/Eisvana_Logo.png',
   },
-  // {
-  //   link: 'https://nomanssky.fandom.com/wiki/Eisvana_Wiki_Scholars',
-  //   imgAlt: 'Eisvana Wiki Scholars logo',
-  //   img: '',
-  // },
 ];
 </script>
 
 <template>
-  <h1 class="title is-spaced">Eisvana Wiki Page Creator</h1>
-  <div class="subtitle is-4">Choose what page you want to create:</div>
+  <p class="q-pt-md text-h5 text-center">Choose what page you want to create:</p>
   <nav
     aria-label="Subpages"
-    class="page-options"
+    class="page-options q-mt-lg q-mb-xl q-mx-auto"
   >
-    <PageLink
+    <QBtn
       v-for="link in links"
-      :url="link.url"
-      :text="link.text"
-      :disabled="link.inactive"
-    />
+      :href="link.url"
+      color="primary"
+      size="xl"
+      no-caps
+      outline
+    >
+      {{ link.text }}
+    </QBtn>
   </nav>
-  <div class="built-by">
-    <div>Brought to you by:</div>
-    <BuiltBy
-      v-for="credit in builtByCredits"
-      :link="credit.link"
-      :text="credit.text"
-      :img="credit.img"
-      :img-alt="credit.imgAlt"
-    />
-  </div>
   <div class="eisvana-images">
     <CivImage
       v-for="image in images"
@@ -131,11 +98,6 @@ const images: CivImageProps[] = [
 </template>
 
 <style scoped lang="scss">
-.built-by {
-  margin: 2em;
-  font-size: 1.25rem;
-}
-
 .eisvana-images {
   display: flex;
   justify-content: center;
@@ -145,7 +107,6 @@ const images: CivImageProps[] = [
 
 .page-options {
   max-width: 800px;
-  margin: 1em auto;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   gap: 1.5em;
