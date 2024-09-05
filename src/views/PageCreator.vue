cf7640deab954fa2ac02d39cb53b53710347542f
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-05 18:58:45 +0200

    add page wrapper component

---
+++ src/views/PageCreator.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row q-gutter-x-lg q-pt-lg">
+  <QCard class="col q-pa-md q-mb-lg">
+    <slot name="input"></slot>
+  </QCard>
+
+  <QCard class="col q-pa-md q-mb-lg monospace">
+    <slot name="output"></slot>
+  </QCard>
+</div>
+
+  <slot></slot>
+</template>
