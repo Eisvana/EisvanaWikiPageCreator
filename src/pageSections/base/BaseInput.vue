a5d2600dbead9a8f87a0781396119583d3e13bbe
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-05 20:45:00 +0200

    POC base page

--- src/pageSections/base/BaseInput.vue
+++ src/pageSections/base/BaseInput.vue
@@ -1,15 +1,118 @@
 <script setup lang="ts">
+import CheckboxInput from '@/components/CheckboxInput.vue';
+import GlyphInput from '@/components/GlyphInput.vue';
 import WikiDataTextInput from '@/components/WikiDataTextInput.vue';
 import { usePageDataStore } from '@/stores/pageData';
 import { storeToRefs } from 'pinia';
 
 const pageData = usePageDataStore();
-const { name } = storeToRefs(pageData);
+const {
+  name,
+  discovered,
+  glyphs,
+  type,
+  discoveredlink,
+  system,
+  planet,
+  moon,
+  axes,
+  farm,
+  geobay,
+  landingpad,
+  terminal,
+  arena,
+  racetrack,
+  censusplayer,
+  censussocial,
+  censusreddit,
+  censusdiscord,
+  censusfriend,
+  censusarrival,
+  censusshow,
+} = storeToRefs(pageData);
 </script>
 
 <template>
   <WikiDataTextInput
     v-model="name"
     label="Name"
+  />
+  <WikiDataTextInput
+    v-model="discoveredlink"
+    label="Builder wiki name"
+  />
+  <WikiDataTextInput
+    v-model="discovered"
+    label="Builder alias if no wiki"
+  />
+  <WikiDataTextInput
+    v-model="system"
+    label="System"
+  />
+  <WikiDataTextInput
+    v-model="planet"
+    label="Planet"
+  />
+  <WikiDataTextInput
+    v-model="moon"
+    label="Moon"
+  />
+  <WikiDataTextInput
+    v-model="axes"
+    label="Planetary Coords"
+  />
+  <GlyphInput v-model="glyphs" />
+  <WikiDataTextInput
+    v-model="type"
+    label="Type of the base"
+  />
+  <div>Features</div>
+  <div>
+    <CheckboxInput
+      v-model="farm"
+      label="Farm"
+    />
+    <CheckboxInput
+      v-model="geobay"
+      label="Geobay"
+    />
+    <CheckboxInput
+      v-model="landingpad"
+      label="Landing Pad"
+    />
+    <CheckboxInput
+      v-model="arena"
+      label="Arena"
+    />
+    <CheckboxInput
+      v-model="terminal"
+      label="Trade Terminal"
+    />
+    <CheckboxInput
+      v-model="racetrack"
+      label="Racetrack"
+    />
+  </div>
+
+  <div>Census</div>
+  <WikiDataTextInput
+    v-model="censusplayer"
+    label="Ingame name"
+  />
+  <WikiDataTextInput
+    v-model="censusreddit"
+    label="Reddit name"
+  />
+  <WikiDataTextInput
+    v-model="censussocial"
+    label="Social media name"
+  />
+  <WikiDataTextInput
+    v-model="censusdiscord"
+    label="Discord name"
+  />
+  <WikiDataTextInput
+    v-model="censusfriend"
+    label="Friend code"
   />
 </template>
--- src/pageSections/base/BaseOutput.vue
+++ src/pageSections/base/BaseOutput.vue
@@ -2,7 +2,7 @@
 import { usePageDataStore } from '@/stores/pageData';
 import { storeToRefs } from 'pinia';
 import { civName as civilized } from '@/variables/civilization';
-import { booleanToYesNo } from '@/helper/booleanFormatting';
+import { computed } from 'vue';
 
 const pageData = usePageDataStore();
 const {
@@ -38,13 +38,19 @@ const {
 } = storeToRefs(pageData);
 
 const censusrenewal = new Date().getUTCFullYear();
+
+const locationSentence = computed(() => {
+  const planetSentence = `planet [[${planet.value}]]`;
+  const moonSentence = `moon [[${moon.value}]] of the`;
+  return moon.value ? `${moonSentence} ${planetSentence}` : planetSentence;
+});
 </script>
 
 <template>
   <div><span v-pre>{{Version|</span>{{ release }}<span v-pre>}}</span></div>
+  <div v-pre>{{Eisvana}}</div>
   <div v-pre>{{Base infobox</div>
   <div>| name = {{ name }}</div>
-
   <div>| image = {{ image }}</div>
   <div>| civilized = {{ civilized }}</div>
   <div>| researchteam = {{ researchteam }}</div>
@@ -62,12 +68,12 @@ const censusrenewal = new Date().getUTCFullYear();
   <div>| mode = {{ mode }}</div>
   <div>| platform = {{ platform }}</div>
   <div>| release = {{ release }}</div>
-  <div>| farm = {{ booleanToYesNo(farm) }}</div>
-  <div>| geobay = {{ booleanToYesNo(geobay) }}</div>
-  <div>| landingpad = {{ booleanToYesNo(landingpad) }}</div>
-  <div>| arena = {{ booleanToYesNo(arena) }}</div>
-  <div>| terminal = {{ booleanToYesNo(terminal) }}</div>
-  <div>| racetrack = {{ booleanToYesNo(racetrack) }}</div>
+  <div>| farm = {{ farm }}</div>
+  <div>| geobay = {{ geobay }}</div>
+  <div>| landingpad = {{ landingpad }}</div>
+  <div>| arena = {{ arena }}</div>
+  <div>| terminal = {{ terminal }}</div>
+  <div>| racetrack = {{ racetrack }}</div>
   <div>| censusplayer = {{ censusplayer }}</div>
   <div>| censussocial = {{ censussocial }}</div>
   <div>| censusreddit = {{ censusreddit }}</div>
@@ -82,7 +88,7 @@ const censusrenewal = new Date().getUTCFullYear();
   <div>==Summary==</div>
   <!--TODO: Add moon sentence-->
   <div>
-    '''{{ name }}''' is a [[Habitable Base|player base]], located on the planet [[{{ planet }}]] in the [[{{ system }}]]
+    '''{{ name }}''' is a [[Habitable Base|player base]], located on the {{ locationSentence }} in the [[{{ system }}]]
     system.
   </div>
   <br />
