fa16edfd7a4d2792ae4f18a709e7147af3c82d0e
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-05 20:44:13 +0200

    generic checkbox input

---
+++ src/components/CheckboxInput.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+}>();
+
+const model = defineModel<string>({ required: true });
+</script>
+
+<template>
+  <QCheckbox
+    v-model="model"
+    :label
+    false-value="No"
+    true-value="Yes"
+  />
+</template>
