82e0b123b4443e8729e07f9ed8bb1c7acbc1170b
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-05 18:58:10 +0200

    make generic text input component

---
+++ src/components/WikiDataTextInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { sanitiseString } from '@/helper/inputSanitisation';
+import { watchEffect } from 'vue';
+
+defineProps<{
+  label: string;
+  error?: boolean;
+  errorMessage?: string;
+}>();
+
+const model = defineModel<string>({ required: true });
+
+watchEffect(() => (model.value = sanitiseString(model.value)));
+</script>
+
+<template>
+  <QInput
+    v-model.trim="model"
+    :error
+    :error-message
+    :label
+    outlined
+  />
+</template>
