f01d3b1f3e46521c68e28d0d3e09ffe2c6ef2fcc
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-08 21:51:15 +0200

    make cards wrap

--- src/views/PageCreator.vue
+++ src/views/PageCreator.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="row q-gutter-x-lg q-pt-lg q-mx-auto page-wrapper">
-    <QCard class="col q-pa-md q-mb-lg">
+  <div class="row wrap q-gutter-x-lg q-pt-lg q-mx-auto page-wrapper">
+    <QCard class="col q-pa-md q-mb-lg card">
       <slot name="input"></slot>
     </QCard>
 
-    <QCard class="col q-pa-md q-mb-lg monospace">
+    <QCard class="col q-pa-md q-mb-lg card monospace">
       <slot name="output"></slot>
     </QCard>
   </div>
@@ -15,5 +15,9 @@
 <style scoped lang="scss">
 .page-wrapper {
   max-width: 1400px;
+
+  .card {
+    min-width: 250px;
+  }
 }
 </style>
