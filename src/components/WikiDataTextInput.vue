2301cedcc4f8be02e13b72469740d53eca84b922
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-08 18:49:22 +0200

    add code for tooltips and help popups

--- src/components/WikiDataTextInput.vue
+++ src/components/WikiDataTextInput.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
 import { sanitiseString } from '@/helper/inputSanitisation';
-import { watchEffect } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import type { QInputProps } from 'quasar';
 
-defineProps<{
+const props = defineProps<{
   label: string;
   error?: boolean;
   errorMessage?: string;
   type?: QInputProps['type'];
+  helpTitle?: string;
+  helpImg?: string;
+  tooltip?: string;
 }>();
 
 const model = defineModel<string>({ required: true });
 
 watchEffect(() => (model.value = sanitiseString(model.value)));
+
+const isOpen = ref(false);
+
+const helperImage = computed(() => `/src/assets/images/${props.helpImg}.webp`);
 </script>
 
 <template>
@@ -22,6 +29,83 @@ watchEffect(() => (model.value = sanitiseString(model.value)));
     :error-message
     :label
     :type
+    dense
     outlined
-  />
+  >
+    <template
+      v-if="helpTitle || tooltip"
+      #append
+    >
+      <QBtn
+        :class="{ 'disable-override': !helpTitle }"
+        :disable="!helpTitle"
+        icon="help"
+        fab-mini
+        flat
+        @click="isOpen = true"
+      >
+        <QTooltip
+          v-if="tooltip"
+          anchor="top middle"
+          self="center middle"
+          >{{ tooltip }}</QTooltip
+        >
+      </QBtn>
+
+      <QDialog v-model="isOpen">
+        <QCard class="card-wrapper full-width">
+          <QCardSection>
+            <h2 class="text-h4 no-margin text-center">{{ helpTitle }}</h2>
+          </QCardSection>
+          <QCardSection class="text-center">
+            <slot></slot>
+          </QCardSection>
+          <QCardSection>
+            <a
+              :href="helperImage"
+              class="image-wrapper link"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <QImg
+                :src="helperImage"
+                alt="Explainer Image"
+              />
+            </a>
+          </QCardSection>
+          <QCardActions class="justify-center">
+            <QBtn
+              v-close-popup
+              class="q-mb-md"
+              label="Close"
+              type="submit"
+              outline
+            />
+          </QCardActions>
+        </QCard>
+      </QDialog>
+    </template>
+  </QInput>
 </template>
+
+<style scoped lang="scss">
+.disable-override {
+  opacity: unset !important;
+
+  &,
+  * {
+    cursor: default !important;
+  }
+}
+
+.card-wrapper {
+  max-width: 640px;
+}
+
+.image-wrapper::after {
+  content: 'Click to enlarge';
+  display: block;
+  text-align: center;
+  padding-block-start: 0.5rem;
+}
+</style>
