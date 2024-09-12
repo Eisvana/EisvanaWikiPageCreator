b90dc5591206f2dc1e897fb9bda61693d1c97d13
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-12 10:20:22 +0200

    register primevue

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,20 @@ import './startup/minimal';
 import App from './App.vue';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.dark',
+    },
+  },
+});
 
 app.mount('#app');
