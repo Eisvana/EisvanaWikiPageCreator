7c013f81c1c9f65f0bc8b4519283195f089dcd05
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-13 15:52:57 +0200

    file upload component

---
+++ src/components/SingleFileUpload.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { useId } from '@/helpers/id';
+import { maxFileSize } from '@/variables/wikiLimits';
+import type { FileUploadSelectEvent } from 'primevue/fileupload';
+import FileUpload from 'primevue/fileupload';
+import InputText from 'primevue/inputtext';
+import InputTableItem from './InputTableItem.vue';
+
+defineProps<{
+  label: string;
+}>();
+
+const model = defineModel<string>({ required: true });
+
+function onUpload(e: FileUploadSelectEvent) {
+  const fileName = e.files[0]?.name;
+  if (!fileName) return;
+  model.value = fileName;
+}
+
+const id = useId('file-upload-');
+</script>
+
+<template>
+  <InputTableItem>
+    <template #label>
+      <label :for="id">{{ label }}</label>
+    </template>
+
+    <template #input>
+      <InputText
+        v-model="model"
+        :id
+        class="mb-2"
+      />
+      <FileUpload
+        :maxFileSize
+        accept="image/*"
+        class="p-button-outlined"
+        mode="basic"
+        auto
+        customUpload
+        @select="onUpload"
+      />
+    </template>
+  </InputTableItem>
+</template>
