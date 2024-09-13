23001ebf306b05318d7e465b71d5d13363bc85e4
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-13 19:08:46 +0200

    create small input component

---
+++ src/components/SmallSanitisedTextInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { TextInputProps } from '@/types/textInputProps';
+import SanitisedTextInput from './SanitisedTextInput.vue';
+
+defineProps<Omit<TextInputProps, 'size'>>();
+
+const model = defineModel<string>({ required: true });
+</script>
+
+<template>
+  <SanitisedTextInput
+    v-model="model"
+    :errorMessage
+    :helpImg
+    :helpTitle
+    :invalid
+    :label
+    :maxlength
+    :tooltip
+    size="small"
+  />
+</template>
