ac7b4cd1a0c0ab48b7b744c28fb2f673d8677f6b
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-16 15:09:48 +0200

    fix left-movement

--- src/components/DialogWrapper.vue
+++ src/components/DialogWrapper.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { syncRefs, useElementBounding, useScrollLock, useWindowSize } from '@vueuse/core';
 import Dialog from 'primevue/dialog';
-import { computed, ref, watch } from 'vue';
+import { computed, ref, watch, watchEffect } from 'vue';
 
 defineProps<{
   closable?: boolean;
@@ -15,7 +15,6 @@ const emit = defineEmits(['show']);
 // There is a bug in PrimeVue that causes layout shifts when a modal is shown: https://github.com/primefaces/primevue/issues/6094
 // This code compensates for this bug
 // This whole scroll killing is only necessary because the <Select> input has some scroll bugs, so scrolling should not happen.
-// There is still a tiny move to the left when the modal is dismissed, but I'm not sure how that can be fixed.
 const doc = ref(document.documentElement);
 const isLocked = useScrollLock(doc);
 
@@ -23,9 +22,13 @@ const { width: windowWidth } = useWindowSize();
 const { width: documentWidth } = useElementBounding(doc);
 
 const scrollBarWidth = computed(() => windowWidth.value - documentWidth.value);
-watch(model, (newVal) => doc.value.style.setProperty('--p-scrollbar-width', `${newVal ? scrollBarWidth.value : 0}px`));
+watch(isLocked, (newVal) =>
+  doc.value.style.setProperty('--p-scrollbar-width', `${newVal ? scrollBarWidth.value : 0}px`)
+);
 
-syncRefs(model, isLocked);
+watchEffect(() => {
+  if (model.value) isLocked.value = true;
+});
 </script>
 
 <template>
@@ -37,6 +40,7 @@ syncRefs(model, isLocked);
     pt:root:class="dialog-wrapper mx-4"
     modal
     @show="emit('show')"
+    @after-hide="isLocked = false"
   >
     <template #header>
       <slot name="header"></slot>
