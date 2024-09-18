4eac2747677ac85d26cdefc33cfc683d1c7fdd45
Lenni009 <Lenni009@users.noreply.github.com>
2024-09-18 14:24:05 +0000

    Deploying to gh-pages from @ Eisvana/EisvanaWikiPageCreator@af15612ca57d5cd6f9f00775ef34fbe0e37add48 🚀

---
+++ pr/739/assets/index-BalgkDp6.js
@@ -0,0 +1,1 @@
+import"./links-c7SUkTPh.js";import"./main-CEXBCbhz.js";import"./runtime-dom.esm-bundler-BSY-fVkX.js";const i="wiki.eisvana.com"===window.location.host,o=document.querySelector('a[href=".."]');o&&i&&(o.href="https://eisvana.com");
