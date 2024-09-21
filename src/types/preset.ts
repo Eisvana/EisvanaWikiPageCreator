691c1ba1fde0d50bc5e58e95e971bd51bba81726
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-21 22:25:05 +0200

    formatting

--- src/types/preset.ts
+++ src/types/preset.ts
@@ -1,13 +1,12 @@
 export interface PresetData {
   discovered: string;
-  discoveredlink: string,
-  documenterName: string,
-  system: string,
-  planet: string,
-  moon: string,
-  platform: string,
-  researchteam: string | null,
-  glyphs: string,
-  wealth: string,
-
+  discoveredlink: string;
+  documenterName: string;
+  system: string;
+  planet: string;
+  moon: string;
+  platform: string;
+  researchteam: string | null;
+  glyphs: string;
+  wealth: string;
 }
