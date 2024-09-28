f15bfb3580ad41fad097597c9ed35bdf78d52139
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-28 14:57:18 +0200

    extract file upload notice

---
+++ src/components/FileUploadNotice.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import WikiLink from './WikiLink.vue';
+import ExternalLink from './ExternalLink.vue';
+</script>
+
+<template>
+  <p>
+    Any pictures you upload here won't be uploaded to the wiki. This is only to autofill the image name. Maximum
+    filesize is 10MB. You can upload your pictures to the wiki on
+    <WikiLink
+      link="Special:Upload?multiple=true"
+      text="Special:Upload"
+    />.
+  </p>
+  <p>
+    You can compress your images with the
+    <ExternalLink
+      link="https://nmscd.com/Image-Compressor/"
+      text="Image Compressor"
+    />.
+  </p>
+</template>
